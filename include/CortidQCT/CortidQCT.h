32d0ad3ebb4a3faef2a0775247d5edfaca2bd950
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-07-30 14:45:26 +0200

    Add basic documentation

--- include/CortidQCT/CortidQCT.h
+++ include/CortidQCT/CortidQCT.h
@@ -1,4 +1,22 @@
+/**
+ * @file      CortidQCT.h.
+ *
+ * @brief     Main header file for CortidQCT library
+ *
+ * @author    Stefan Reinhold
+ * @copyright Copyright (C) 2018 Stefan Reinhold  -- All Rights Reserved.
+ *            You may use, distribute and modify this code under the terms of the
+ *            AFL 3.0 license; see LICENSE for full license details.
+ */
+
 #pragma once
 
 #include "src/MeshFitter.h"
+
+/**
+ * @brief Name namespace for CortidQCT library
+ */
+namespace CortidQCT {
+
+} // namespace CortidQCT
 
