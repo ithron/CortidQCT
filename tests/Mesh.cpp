512cc386dd7088d8a189db48fc005dce74f8b9e4
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-08-01 17:56:58 +0200

    Add basic test cases for Mesh::loadFromFile()

--- tests/Mesh.cpp
+++ tests/Mesh.cpp
@@ -6,6 +6,15 @@
 
 using namespace CortidQCT;
 
+#ifndef CortidQCT_DATADIR
+#  error "No data dir given"
+#endif
+
+static std::string const mesh1 =
+    std::string(CortidQCT_DATADIR) + "/SimpleVertebra.off";
+static std::string const labels1 =
+    std::string(CortidQCT_DATADIR) + "/SimpleVertebra-labels.txt";
+
 TEST(Mesh, EmptyMeshIsMepty) {
   Mesh<double> const mesh;
 
@@ -14,3 +23,28 @@ TEST(Mesh, EmptyMeshIsMepty) {
   ASSERT_EQ(0, mesh.triangleCount());
 }
 
+TEST(Mesh, LoadFromFileThrowsIfFileDesNotExist) {
+
+  Mesh<double> mesh;
+
+  // Generate temp file name
+  auto const file = std::string{"some-non-existent-file.off"};
+
+  ASSERT_THROW(mesh.loadFromFile(file, file), std::invalid_argument);
+  ASSERT_THROW(mesh.loadFromFile(file), std::invalid_argument);
+}
+
+TEST(Mesh, WriteToFileDoesNotThrowOnEmptyMesh) {
+  Mesh<double> const mesh;
+
+  ASSERT_NO_THROW(mesh.writeToFile("does-not-matter.obj", "dummy.dat"));
+  ASSERT_NO_THROW(mesh.writeToFile("does-not-matter.obj"));
+}
+
+TEST(Mesh, MeashLoadFromFileDoesNotThrowOnSuccess) {
+
+  Mesh<double> mesh;
+
+  ASSERT_NO_THROW(mesh.loadFromFile(mesh1, labels1));
+  ASSERT_NO_THROW(mesh.loadFromFile(mesh1));
+}
