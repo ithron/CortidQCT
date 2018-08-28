30cc8423b64691e4ac27b4a30df80a518e333ca9
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-08-28 13:09:11 +0200

    Add test cases for loading VoxelVolume objects from bst files

--- tests/VoxelVolume.cpp
+++ tests/VoxelVolume.cpp
@@ -25,6 +25,9 @@ using namespace CortidQCT;
 static std::string const file1 =
     std::string(CortidQCT_DATADIR) + "/ascendingSlices.bst";
 
+static constexpr auto volumeSize1 = VolumeSize{20, 40, 10};
+static constexpr auto voxelSize1 = VoxelSize{0.25f, 0.5f, 1.0f};
+
 TEST(VoxelVolume, EmptyVolumeIsEmpty) {
   VoxelVolume const emptyVolume;
 
@@ -33,3 +36,50 @@ TEST(VoxelVolume, EmptyVolumeIsEmpty) {
   ASSERT_EQ(0, emptyVolume.volumeSize().height);
   ASSERT_EQ(0, emptyVolume.volumeSize().depth);
 }
+
+TEST(VoxelVolume, LoadBST) {
+  auto volume = VoxelVolume();
+
+  ASSERT_NO_THROW(volume.loadFromFile(file1));
+
+  ASSERT_FALSE(volume.isEmpty());
+
+  ASSERT_EQ(volumeSize1, volume.volumeSize());
+
+  ASSERT_FLOAT_EQ(voxelSize1.width, volume.voxelSize().width);
+  ASSERT_FLOAT_EQ(voxelSize1.height, volume.voxelSize().height);
+  ASSERT_FLOAT_EQ(voxelSize1.depth, volume.voxelSize().depth);
+
+  volume.withUnsafeDataPointer([&](float const *dataPtr) {
+    auto const size = volume.volumeSize();
+    auto const data =
+        gsl::make_span(dataPtr, gsl::narrow<std::ptrdiff_t>(size.linear()));
+
+    auto idx = 0u;
+    for (auto &&val : data) {
+      auto const x = idx % size.width;
+      auto const y = (idx / size.width) % size.height;
+      auto const z = idx / (size.height * size.width);
+
+      auto const ref = -2000.f + static_cast<float>(y) * 0.5f +
+                       static_cast<float>(x) * 20.f +
+                       static_cast<float>(z) * 400.f;
+
+      ASSERT_FLOAT_EQ(ref, val);
+      ++idx;
+    }
+  });
+}
+
+TEST(VoxelVolume, LoadFromFileThrowsOnUnknownFormat) {
+
+  ASSERT_THROW(VoxelVolume{"non-existant-file.unsupportedExtension"},
+               std::invalid_argument);
+}
+
+TEST(VoxelVolume, LoadFromFileThrowsOnNonExistentFile) {
+
+  ASSERT_THROW(VoxelVolume{"non-existant-file.bst"},
+               std::invalid_argument);
+}
+
