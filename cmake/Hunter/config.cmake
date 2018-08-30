f57795fc6137d4456c3fa9a891e33af141c033dc
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-08-30 14:33:28 +0200

    Use submodule version of yaml-cpp

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,13 @@
 option(CortidQCT_GIT_SELF "Turn of if building examples or tools as stand-alone project" OFF)
 
+hunter_config(yaml-cpp
+  GIT_SUBMODULE "Dependencies/yaml-cpp"
+  CMAKE_ARGS
+    YAML_CPP_BUILD_TESTS=OFF
+    YAML_CPP_BUILD_TOOLS=OFF
+    YAML_CPP_BUILD_CONTRIB=OFF
+)
+
 if (CortidQCT_GIT_SELF)
   hunter_config(CortidQCT GIT_SELF)
 endif()
