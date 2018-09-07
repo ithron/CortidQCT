1620e0112c38708c578d52fcbbf79cadbda7fd6a
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-09-07 17:34:13 +0200

    Update Microsoft.GSL to latest hunterized version
    Also renamed submodule to Microsoft.GSL

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,12 @@
 option(CortidQCT_GIT_SELF "Turn of if building examples or tools as stand-alone project" OFF)
 
+hunter_config(Microsoft.GSL
+  GIT_SUBMODULE "Dependencies/Microsoft.GSL"
+  CMAKE_ARGS
+    GSL_TEST=OFF
+    GSL_CXX_STANDARD=17
+)
+
 hunter_config(yaml-cpp
   GIT_SUBMODULE "Dependencies/yaml-cpp"
   CMAKE_ARGS
@@ -11,13 +18,6 @@ hunter_config(yaml-cpp
 if (CortidQCT_GIT_SELF)
   hunter_config(CortidQCT GIT_SELF)
 endif()
-
-hunter_config(Microsoft.GSL
-  GIT_SUBMODULE "Dependencies/GSL"
-  CMAKE_ARGS
-    GSL_TEST=OFF
-    GSL_CXX_STANDARD=17
-)
 
 hunter_config(libigl
   GIT_SUBMODULE "Dependencies/libigl"
