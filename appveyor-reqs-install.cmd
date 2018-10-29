6e6e3c74c3a4017b02ff88e42e1a72985eb780c3
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-10-29 17:27:50 +0100

    Fix ninja caching

--- appveyor-reqs-install.cmd
+++ appveyor-reqs-install.cmd
@@ -3,12 +3,12 @@
 ::###########################################################################
 :::: Install Ninja
 ::::###########################################################################
+@set PATH=C:\projects\deps\ninja;%PATH%
 if NOT EXIST ninja (
   appveyor DownloadFile https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip -FileName ninja.zip
   7z x ninja.zip -oC:\projects\deps\ninja > nul
   rm ninja.zip
 )
-@set PATH=C:\projects\deps\ninja;%PATH%
 ninja --version
 
 @echo off
