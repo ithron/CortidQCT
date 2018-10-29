30bfe5d8153c4d00ad61f8ba25db22bf049459bf
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-10-29 15:37:24 +0100

    Improve usgae message

--- app/CLI.cpp
+++ app/CLI.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
 
   if (argc != 4) {
     std::cerr << "Usage: " << argv[0]
-              << " configurationFile inputVolume outputMesh" << std::endl;
+              << " <configurationFile> <inputVolume> <outputMesh> [outputLabels]" << std::endl;
     return EXIT_FAILURE;
   }
 
