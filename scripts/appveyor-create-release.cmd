ce22e347e0519504d4691d19e5be3a7b00c6bcce
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-02 14:19:34 +0100

    Copy data directory into the release archive

--- scripts/appveyor-create-release.cmd
+++ scripts/appveyor-create-release.cmd
@@ -4,6 +4,7 @@ if "%APPVEYOR_REPO_TAG%" == "true" (
   md "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab"
   cp app/CortidQCT_CLI.exe "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
   cp -r "%APPVEYOR_BUILD_FOLDER%\\matlab\\+CortidQCT" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\"
+  cp -r "%APPVEYOR_BUILD_FOLDER%\\data" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
   cp "matlab\\floatVector2base64.mexw64" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\+CortidQCT\\" 
   7z a -tzip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64
   appveyor PushArtifact CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip
