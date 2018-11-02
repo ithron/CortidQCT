aa15017e9c17e93b612d69fd50e29bbdce5f294b
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-02 13:38:36 +0100

    Fix release creation script

--- scripts/appveyor-create-release.cmd
+++ scripts/appveyor-create-release.cmd
@@ -1,8 +1,9 @@
 
 if "%APPVEYOR_REPO_TAG%" == "true" (
   md CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64
+  md "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab"
   cp app/CortidQCT_CLI.exe "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
-  cp -r "%APPVEYOR_BUILD_FOLDER%\\matlab" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
+  cp -r "%APPVEYOR_BUILD_FOLDER%\\matlab\\+CortidQCT" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\"
   cp "matlab\\floatVector2base64.mexw64" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\+CortidQCT\\" 
   7z a -tzip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64
   appveyor PushArtifact CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip
