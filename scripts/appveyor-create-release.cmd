9b8fefa6bf37872d47311d58f8013b5f668bfa1c
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-02 15:27:28 +0100

    Add readme and license to binary releases

--- scripts/appveyor-create-release.cmd
+++ scripts/appveyor-create-release.cmd
@@ -6,9 +6,12 @@ if "%APPVEYOR_REPO_TAG%" == "true" (
   md "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab"
   cp app/CortidQCT_CLI.exe "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\bin"
   cp lib\CortidQCT-Core.lib "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\lib"
+  cp README.md "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
+  cp LICENSE "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
   cp -r "%APPVEYOR_BUILD_FOLDER%\\matlab\\+CortidQCT" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\"
   cp -r "%APPVEYOR_BUILD_FOLDER%\\data" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
   cp -r "%APPVEYOR_BUILD_FOLDER%\\include" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
+  cp -r "%APPVEYOR_BUILD_FOLDER%\\images" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\"
   cp "matlab\\floatVector2base64.mexw64" "CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64\\matlab\\+CortidQCT\\" 
   7z a -tzip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64
   appveyor PushArtifact CortidQCT-%APPVEYOR_REPO_TAG_NAME%-Windows-x64.zip
