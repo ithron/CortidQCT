cebcbc60ede92b16d46a9c78714103bc2e251520
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-05 15:25:31 +0100

    Fix exit code, add comments

--- bindings/C/examples/cli.c
+++ bindings/C/examples/cli.c
@@ -32,20 +32,24 @@ int main(int argc, char **argv) {
 
   CQCT_Error error = NULL;
 
+  // Load voxel volume from file
   CQCT_VoxelVolume volume = CQCT_createVoxelVolume(argv[2], &error);
   if (volume == NULL) {
     fprintf(stderr, "Error: %s\n", CQCT_errorMessage(error));
     goto EXIT;
   }
 
+  // Load MeshFitter (configuration) from file
   CQCT_MeshFitter fitter = CQCT_createMeshFitter(argv[1], &error);
   if (fitter == NULL) {
     fprintf(stderr, "Error: %s\n", CQCT_errorMessage(error));
     goto EXIT;
   }
 
+  // Call fitting function
   CQCT_MeshFitterResult result = CQCT_meshFitterFit(fitter, volume);
 
+  // Ger result deformed mesh
   CQCT_Mesh deformedMesh = CQCT_meshFitterResultMesh(result);
   if (deformedMesh == NULL) {
     fprintf(stderr, "Fitting failed with unkown error\n");
@@ -54,12 +58,16 @@ int main(int argc, char **argv) {
 
   const char *labelFilename = argc == 5 ? argv[4] : "/dev/null";
 
+  // Write result mehr to file
   if (!CQCT_meshAndLabelsWriteToFile(deformedMesh, argv[3], labelFilename,
                                      &error)) {
     fprintf(stderr, "Failed to write result mesh: %s\n",
             CQCT_errorMessage(error));
     goto EXIT;
   }
+
+  // Everything went fine
+  exitCode = EXIT_SUCCESS;
 
 EXIT:
   // Release all created objects, doesn't do anything when passed NULL.
