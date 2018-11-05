94d5c23a7ca75246481fdcf533a17fb93c6c8727
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-05 16:30:27 +0100

    Add missing cstring include
    GCC complains otherwise.

--- bindings/C/src/CortidQCT-Common.h
+++ bindings/C/src/CortidQCT-Common.h
@@ -5,43 +5,42 @@
 #include <CortidQCT/CortidQCT.h>
 
 #include <cstddef>
+#include <cstring>
 
 namespace CortidQCT {
 namespace Internal {
 namespace C {
 
-typedef void(*Deleter)(Id);
+typedef void (*Deleter)(Id);
 
 CQCT_EXTERN struct Control {
   std::size_t retainCount = 1;
   Deleter deleter;
 };
 
-template<class T> struct GenericObjectWrapper {
+template <class T> struct GenericObjectWrapper {
   Control control;
   T *objPtr = nullptr;
-  
+
   ~GenericObjectWrapper() {
-    if (objPtr != nullptr) {
-      delete objPtr;
-    }
+    if (objPtr != nullptr) { delete objPtr; }
   }
 };
-  
-template<class T> void genericDeleter(Id obj) {
+
+template <class T> void genericDeleter(Id obj) {
   auto objWrapperPtr = static_cast<GenericObjectWrapper<T> *>(obj);
   delete objWrapperPtr;
 }
 
-template<class T, class ...Args> Id constructObject(Args &&...args) {
+template <class T, class... Args> Id constructObject(Args &&... args) {
   auto wrapperPtr = new GenericObjectWrapper<T>;
-  
+
   wrapperPtr->control.deleter = &genericDeleter<T>;
   wrapperPtr->objPtr = new T{std::forward<Args>(args)...};
-  
+
   return static_cast<Id>(wrapperPtr);
 }
-    
+
 } // namespace C
 } // namespace Internal
 } // namespace CortidQCT
