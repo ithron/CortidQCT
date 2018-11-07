ef387e73fe090ca2157aa9d337b2f1529c6f6cfb
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-07 13:36:47 +0100

    Update Mesh implementation

--- bindings/matlab/Mesh.m
+++ bindings/matlab/Mesh.m
@@ -1,35 +1,83 @@
-classdef Mesh < CortidQCT_Object
-    %MESH A triangular mesh
+classdef Mesh < ObjectBase
+  %MESH A triangular mesh
+
+  properties (Dependent)
+    vertexCount
+    triangleCount
+    Vertices
+    Indices
+    Labels
+  end
+
+  methods
+
+    % Constructor
+    function obj = Mesh()
+      handle = ObjectBase.call('createMesh');
+      obj@ObjectBase(handle);
+    end
+
+    %%%%%%%%%%%%%%%%%
+    %% Getters
+    %
+
+    function vertexCount = get.vertexCount(obj)
+      vertexCount = ObjectBase.call('meshVertexCount', obj.handle);
+    end
+
+    function triangleCount = get.triangleCount(obj)
+      triangleCount = ObjectBase.call('meshTriangleCount', obj.handle);
+    end
+
+    function Vertices = get.Vertices(obj)
+      vBuffer = libpointer('singlePtr', zeros(3, obj.vertexCount, 'single'));
+      result = ObjectBase.call('meshCopyVertices', obj.handle, vBuffer);
+      assert(result == 4 * length(vBuffer.Value(:)), "Size mismatch");
+      Vertices = vBuffer.Value;
+    end
+
+    function Indices = get.Indices(obj)
+      iBuffer = libpointer('longPtr', zeros(3, obj.triangleCount, 'int64'));
+      result = ObjectBase.call('meshCopyTriangles', obj.handle, iBuffer);
+      assert(result == 8 * length(iBuffer.Value(:)), "Size mismatch");
+      Indices = iBuffer.Value;
+    end
+
+    function Labels = get.Labels(obj)
+      lBuffer = libpointer('uint32Ptr', zeros(obj.vertexCount, 1, 'uint32'));
+      result = ObjectBase.call('meshCopyLabels', obj.handle, lBuffer);
+      assert(result == 4 * length(lBuffer.Value(:)), "Size mismatch");
+      Labels = lBuffer.Value;
+    end
+
+    %%%%%%%%%%%%%%%%%
+    %% Modifyers
+    %
+
+    function success = loadFromFile(obj, meshFilename, varargin)
+      err = Error;
+      
+      if nargin == 2
+        result = ObjectBase.call('loadMesh', obj.handle, meshFilename, err.pointer);
+      else
+        labelFilename = varargin{1};
+        result = ObjectBase.call('loadMeshAndLabels', obj.handle, meshFilename, labelFilename, err.pointer);
+      end
+      if result == 0
+        error('Failed to load mesh: %s', err.message);
+      end
+      success = result == 1;
+    end
+
+  end % methods
+  
+  methods (Static)
     
-    properties (Dependent)
-        vertexCount
-        triangleCount
-        Vertices
-        Indices
+    function mesh = fromFile(filename, varargin)
+      mesh = Mesh;
+      mesh.loadFromFile(filename, varargin{:});
     end
     
-    methods
-        
-        function obj = Mesh()
-            prepareLibrary;
-            handle = calllib('CortidQCT', 'CQCT_createMesh');
-            obj@CortidQCT_Object(handle);
-            
-        end
-        
-        function vertexCount = get.vertexCount(obj)
-            prepareLibrary;
-            
-            vertexCount = calllib('CortidQCT', 'CQCT_meshVertexCount', obj.handle);
-        end
-        
-        function triangleCount = get.triangleCount(obj)
-            prepareLibrary;
-            
-            triangleCount = calllib('CortidQCT', 'CQCT_meshTriangleCount', obj.handle);
-        end
-        
-        
-    end
+  end % Static methods
 end
 
