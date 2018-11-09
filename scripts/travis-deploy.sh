eb1ac702dc8e5f1ebb45b948298187cb2afa99f2
Stefan Reinhold <stefan@sreinhold.com>
2018-11-09 12:49:32 +0100

    macOS deployment script

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -11,9 +11,19 @@ if [[ -v TRAVIS_TAG ]]; then
   cp -r images install/
   cp -r data install/
 
-  REL_DIR=CortidQCT-${TRAVIS_TAG}-x86_64-linux-gnu-ubuntu-18.04
+  if [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
+    REL_DIR=CortidQCT-${TRAVIS_TAG}-x86_64-linux-gnu-ubuntu-18.04
+  elif [[ "${TRAVIS_OS_NAME}" = "osx" ]]; then
+    REL_DIR=CortidQCT-${TRAVIS_TAG}-x86_64-macOS-10.14
+  fi
+
   mv install ${REL_DIR}
-  tar czf ${REL_DIR}.tar.gz ${REL_DIR}
+
+  if [[ "${TRAVIS_OS_NAME}" = "linux" ]]; then
+    tar czf ${REL_DIR}.tar.gz ${REL_DIR}
+  elif [[ "${TRAVIS_OS_NAME}" = "osx" ]]; then
+    zip -r ${REL_DIR}.zip ${REL_DIR}
+  fi
 
 fi
 
