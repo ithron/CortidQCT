1a523cf074a5b77d67b7e83a8c1a25d606cc1746
Travis CI <travis@travis-ci.org>
2018-11-13 08:36:28 +0000

    Deploy code docs to GitHub Pages Travis build: 263
    Commit: dad2b270282df9283cd12834f4e9c9e9e58fee52

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['hash_3c_20cortidqct_3a_3acolorrgb_20_3e',['hash&lt; CortidQCT::ColorRGB &gt;',['../structstd_1_1hash_3_01CortidQCT_1_1ColorRGB_01_4.html',1,'std']]],
   ['height',['height',['../structCortidQCT_1_1VolumeSize.html#a7558c3ad1e18ba8f3d1f490408bdf15a',1,'CortidQCT::VolumeSize::height()'],['../structCortidQCT_1_1VoxelSize.html#ada3d0014ba4585c8351ceb366ab3af93',1,'CortidQCT::VoxelSize::height()']]],
-  ['hiddenstate',['HiddenState',['../structCortidQCT_1_1MeshFitter_1_1HiddenState.html',1,'CortidQCT::MeshFitter']]]
+  ['hiddenstate',['HiddenState',['../structCortidQCT_1_1MeshFitter_1_1State_1_1HiddenState.html',1,'CortidQCT::MeshFitter::State']]]
 ];
--- html/search/all_e.js
+++ html/search/all_e.js
@@ -6,7 +6,7 @@ var searchData=
   ['scalar',['Scalar',['../classCortidQCT_1_1Mesh.html#a81c02ff8c5040d90b417c8541e2e076b',1,'CortidQCT::Mesh']]],
   ['sigmae',['sigmaE',['../structCortidQCT_1_1MeshFitter_1_1Configuration.html#a8f0b4875224ee7e8857a01c767ea1fc1',1,'CortidQCT::MeshFitter::Configuration']]],
   ['sigmas',['sigmaS',['../structCortidQCT_1_1MeshFitter_1_1Configuration.html#ae3350f4660fb532a2a58a76bc95ffcaf',1,'CortidQCT::MeshFitter::Configuration']]],
-  ['size',['Size',['../structSize.html',1,'Size'],['../classCortidQCT_1_1VoxelVolume.html#abf15c9499b4b854e4d8c47d34dfb5f04',1,'CortidQCT::VoxelVolume::size()'],['../classCortidQCT_1_1Mesh.html#ac2af31ab3131a4ce0a6ed86b6e29d8f9',1,'CortidQCT::Mesh::Size()']]],
+  ['size',['Size',['../structSize.html',1,'Size'],['../classCortidQCT_1_1Mesh.html#ac2af31ab3131a4ce0a6ed86b6e29d8f9',1,'CortidQCT::Mesh::Size()'],['../classCortidQCT_1_1VoxelVolume.html#abf15c9499b4b854e4d8c47d34dfb5f04',1,'CortidQCT::VoxelVolume::size()']]],
   ['slicespacing',['sliceSpacing',['../classCortidQCT_1_1MeasurementModel.html#aafdeea99c3b363285999e19354ca0602',1,'CortidQCT::MeasurementModel']]],
   ['state',['State',['../structCortidQCT_1_1MeshFitter_1_1State.html',1,'CortidQCT::MeshFitter']]],
   ['stride',['stride',['../structCortidQCT_1_1DiscreteRange.html#a33be3e2f7a4dd1b26a8184c79b39e91c',1,'CortidQCT::DiscreteRange']]],
