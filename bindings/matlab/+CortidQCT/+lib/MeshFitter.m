25f1af7ad71c2d81a376f0f0f297518d65833245
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-11-14 16:17:34 +0100

    Add single step function to MeshFitter

--- bindings/matlab/+CortidQCT/+lib/MeshFitter.m
+++ bindings/matlab/+CortidQCT/+lib/MeshFitter.m
@@ -65,6 +65,111 @@ classdef MeshFitter < CortidQCT.lib.ObjectBase
         success = true;
       end
     end
+
+    function success = volumeSamplingStep(obj, state)
+      if not(isa(state, 'CortidQCT.lib.MeshFitterState'))
+        error('State must be an instance of MeshFitterState. Call init() first!');
+      end
+
+      import CortidQCT.lib.ObjectBase;
+      import CortidQCT.lib.Error;
+
+      err = Error;
+
+      res = ObjectBase.call('meshFitterVolumeSamplingStep', ...
+        obj.handle, state.pointer, err.pointer);
+
+      if res == 0
+        error('volumeSamplingStep failed: %s', err.message);
+        success = false;
+      else
+        success = true;
+      end
+    end
+
+    function success = optimalDisplacementStep(obj, state)
+      if not(isa(state, 'CortidQCT.lib.MeshFitterState'))
+        error('State must be an instance of MeshFitterState. Call init() first!');
+      end
+
+      import CortidQCT.lib.ObjectBase;
+      import CortidQCT.lib.Error;
+
+      err = Error;
+
+      res = ObjectBase.call('meshFitterOptimalDisplacementStep', ...
+        obj.handle, state.pointer, err.pointer);
+
+      if res == 0
+        error('optimalDisplacementStep failed: %s', err.message);
+        success = false;
+      else
+        success = true;
+      end
+    end
+
+    function success = optimalDeformationStep(obj, state)
+      if not(isa(state, 'CortidQCT.lib.MeshFitterState'))
+        error('State must be an instance of MeshFitterState. Call init() first!');
+      end
+
+      import CortidQCT.lib.ObjectBase;
+      import CortidQCT.lib.Error;
+
+      err = Error;
+
+      res = ObjectBase.call('meshFitterOptimalDeformationStep', ...
+        obj.handle, state.pointer, err.pointer);
+
+      if res == 0
+        error('optimalDeformationStep failed: %s', err.message);
+        success = false;
+      else
+        success = true;
+      end
+    end
+
+    function success = logLikelihoodStep(obj, state)
+      if not(isa(state, 'CortidQCT.lib.MeshFitterState'))
+        error('State must be an instance of MeshFitterState. Call init() first!');
+      end
+
+      import CortidQCT.lib.ObjectBase;
+      import CortidQCT.lib.Error;
+
+      err = Error;
+
+      res = ObjectBase.call('meshFitterLogLikelihoodStep', ...
+        obj.handle, state.pointer, err.pointer);
+
+      if res == 0
+        error('VolumeSamplingStep failed: %s', err.message);
+        success = false;
+      else
+        success = true;
+      end
+    end
+
+    function success = convergenceTestStep(obj, state)
+      if not(isa(state, 'CortidQCT.lib.MeshFitterState'))
+        error('State must be an instance of MeshFitterState. Call init() first!');
+      end
+
+      import CortidQCT.lib.ObjectBase;
+      import CortidQCT.lib.Error;
+
+      err = Error;
+
+      res = ObjectBase.call('meshFitterConvergenceTestStep', ...
+        obj.handle, state.pointer, err.pointer);
+
+      if res == 0
+        error('convergenceTestStep failed: %s', err.message);
+        success = false;
+      else
+        success = true;
+      end
+    end
     
   end
 end
