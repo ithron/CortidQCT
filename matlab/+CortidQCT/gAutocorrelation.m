8a6ca953cd64905337feaca1c61b3a4601646e97
Stefan Reinhold <sre@informatik.uni-kiel.de>
2018-12-11 12:13:44 +0100

    Add license text

--- matlab/+CortidQCT/gAutocorrelation.m
+++ matlab/+CortidQCT/gAutocorrelation.m
@@ -1,6 +1,11 @@
 function c = gAutocorrelation(s, t, theta, GTable)
 %GAUTOCORRELATION Computes the autocorrelation coefficients of g at s and t
 
+% This file is part of the 'CortidQCT' project.
+% Author: Stefan Reinhold
+% Copyright: Copyright (C) 2018 Stefan Reinhold  -- All Rights Reserved.¬
+%            You may use, distribute and modify this code under the terms of¬
+%            the AFL 3.0 license; see LICENSE for full license details.
 
 if isvector(s) && isvector(t)
   if isrow(s)
