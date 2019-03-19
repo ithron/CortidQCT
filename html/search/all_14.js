29187b992c7f9db6d57619f00e9a9f29ac097a76
Travis CI <travis@travis-ci.org>
2019-03-19 09:46:48 +0000

    Deploy code docs to GitHub Pages Travis build: 347
    Commit: 17ecc814132ae622b9b1a8e97e84ef333f86c866

--- html/search/all_14.js
+++ html/search/all_14.js
@@ -3,6 +3,7 @@ var searchData=
   ['v',['v',['../structCQCT__BarycentricPoint__t.html#a764b1b7b1e09d8fd4b7cbbbc870d8feb',1,'CQCT_BarycentricPoint_t']]],
   ['value_5ftype',['value_type',['../structCortidQCT_1_1DiscreteRange.html#aee5b5a6e0b971b248cfdd66a40724840',1,'CortidQCT::DiscreteRange']]],
   ['valuetype',['ValueType',['../classCortidQCT_1_1VoxelVolume.html#af9e11e71d4fc998cc3c806940b446e58',1,'CortidQCT::VoxelVolume']]],
+  ['version_2eh',['Version.h',['../Version_8h.html',1,'']]],
   ['vertexcount',['vertexCount',['../classCortidQCT_1_1Mesh.html#a00230e71237643779b241188e7acd84d',1,'CortidQCT::Mesh']]],
   ['vertexnormals',['vertexNormals',['../structCortidQCT_1_1MeshFitter_1_1Result.html#aa79550b8c62b3654ea279790f03c9a46',1,'CortidQCT::MeshFitter::Result']]],
   ['volumesampler',['VolumeSampler',['../classCortidQCT_1_1VolumeSampler.html',1,'CortidQCT']]],
@@ -14,7 +15,7 @@ var searchData=
   ['voxelsize',['VoxelSize',['../structVoxelSize.html',1,'VoxelSize'],['../classCortidQCT_1_1VoxelVolume.html#abaefe2a77728ffa149c0685055c3e8cd',1,'CortidQCT::VoxelVolume::voxelSize()']]],
   ['voxelsize',['VoxelSize',['../structCortidQCT_1_1VoxelSize.html',1,'CortidQCT']]],
   ['voxelsize_2eh',['VoxelSize.h',['../VoxelSize_8h.html',1,'']]],
-  ['voxelvolume',['VoxelVolume',['../classCortidQCT_1_1VoxelVolume.html',1,'CortidQCT']]],
   ['voxelvolume',['VoxelVolume',['../classCortidQCT_1_1VoxelVolume.html#a0b2a3dd9a735c181e226d65994b79eaa',1,'CortidQCT::VoxelVolume::VoxelVolume() noexcept'],['../classCortidQCT_1_1VoxelVolume.html#a6a4e5fa5b0b42b7422964dfaa5ba2222',1,'CortidQCT::VoxelVolume::VoxelVolume(std::string const &amp;filename)']]],
+  ['voxelvolume',['VoxelVolume',['../classCortidQCT_1_1VoxelVolume.html',1,'CortidQCT']]],
   ['voxelvolume_2eh',['VoxelVolume.h',['../VoxelVolume_8h.html',1,'']]]
 ];
--- html/search/files_b.js
+++ html/search/files_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['version_2eh',['Version.h',['../Version_8h.html',1,'']]],
   ['volumesize_2eh',['VolumeSize.h',['../VolumeSize_8h.html',1,'']]],
   ['voxelsize_2eh',['VoxelSize.h',['../VoxelSize_8h.html',1,'']]],
   ['voxelvolume_2eh',['VoxelVolume.h',['../VoxelVolume_8h.html',1,'']]]
