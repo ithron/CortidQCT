150ff62a15a23af229b59ffd4ac5e73c2ad34808
Travis CI <travis@travis-ci.org>
2019-03-21 12:53:12 +0000

    Deploy code docs to GitHub Pages Travis build: 364
    Commit: 5700d2cccfe06c0705bd3062c0540a2be72c5b0c

--- html/search/classes_b.js
+++ html/search/classes_b.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['volumesampler',['VolumeSampler',['../classCortidQCT_1_1VolumeSampler.html',1,'CortidQCT']]],
   ['volumesize',['VolumeSize',['../structCortidQCT_1_1VolumeSize.html',1,'CortidQCT']]],
-  ['voxelsize',['VoxelSize',['../structVoxelSize.html',1,'']]],
   ['voxelsize',['VoxelSize',['../structCortidQCT_1_1VoxelSize.html',1,'CortidQCT']]],
+  ['voxelsize',['VoxelSize',['../structVoxelSize.html',1,'']]],
   ['voxelvolume',['VoxelVolume',['../classCortidQCT_1_1VoxelVolume.html',1,'CortidQCT']]]
 ];
